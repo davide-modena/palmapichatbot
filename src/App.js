bbd303981cb004de2efae5881a12ab951cb9218b
davide-modena <burtoninwonderland56@gmail.com>
2023-12-14 22:38:21 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <div className="chat-bot">
+      <div className="chat-header">
+        <div className="info-container">
+          <h3>Chat with</h3>
+          <h2>PaLM 2 Bot</h2>
+        </div>
+        //svg
+      </div>
+
+      <div className="feed">
+        <div className="question bubble"></div>
+        <div className="response bubble"></div>
+      </div>
+
+      {/* <textarea value={} onChange={}/> */}
+      {/* <button onClick={}>></button> */}
+    </div>
+  );
+}
+
+export default App;
